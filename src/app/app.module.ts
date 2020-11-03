f55bffb02e18413ade7c791ec2ecd0f19040b00a
petershivachi <58518220+petershivachi@users.noreply.github.com>
2020-11-03 16:36:37 +0300

    angular-todo app committed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todos/todo/todo.component';
+import { TodoItemComponent } from './components/todo-item/todo-item/todo-item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { AddTodoComponent } from './components/add-todo/add-todo/add-todo.component';
+import { from } from 'rxjs';
+import { AboutComponent } from './components/pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/todo-item/todo-item/todo-item.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { from } from 'rxjs';
+import { Todo } from '../../../models/Todo'
+import {TodoService} from '../../../services/todo.service'
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+  }
+
+  //set dynamic classes
+
+  setClasses(){
+    let classes = {
+      todo: true,
+      'is-complete ': this.todo.completed
+    }
+    return classes
+  }
+
+  onToggle(todo){
+    //toggle in the UI
+    todo.completed = !todo.completed
+
+    //toggle on the server
+
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
+  }
+
+  onDelete(todo){
+    this.deleteTodo.emit(todo)
+  }
+
+}
